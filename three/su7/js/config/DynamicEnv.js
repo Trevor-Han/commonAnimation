5c0f95efa4bd00ecccd7c7a18c728eda5c1bd356
Trevor <hanmingbin@sinosoft.com.cn>
2024-07-09 22:30:17 +0800

    添加噪声，优化动画效果

---
+++ three/su7/js/config/DynamicEnv.js
@@ -0,0 +1,57 @@
+import * as THREE from "../3d/three.module.js";
+import {vertexShader as dynamicEnvVertexShader } from "../shaders/DynamicEnv/vert.js";
+import {fragmentShader as dynamicEnvFragmentShader } from "../shaders/DynamicEnv/frag.js";
+import FullScreenQuad from "./FullScreenQuad.js";
+import FBO from "./FBO.js";
+
+export default class DynamicEnv{
+    renderer;
+    fbo;
+    material;
+    constructor(renderer,config) {
+        const { envmap1, envmap2 } = config;
+        const envData = envmap1?.source.data;
+
+        this.fbo = new FBO({},{
+            width: envData.width,
+            height: envData.height,
+        });
+        this.envMap.mapping = THREE.EquirectangularReflectionMapping;
+
+        this.material = new THREE.ShaderMaterial({
+            vertexShader: dynamicEnvVertexShader,
+            fragmentShader: dynamicEnvFragmentShader,
+            uniforms: {
+                uEnvmap1: {
+                    value: envmap1,
+                },
+                uEnvmap2: {
+                    value: envmap2,
+                },
+                uWeight: {
+                    value: 0,
+                },
+                uIntensity: {
+                    value: 1,
+                },
+            },
+        });
+        this.quad = new FullScreenQuad(this.material)
+
+        this.renderer = renderer
+    }
+    get envMap() {
+        return this.fbo.rt.texture;
+    }
+    update(renderer) {
+        renderer.setRenderTarget(this.fbo);
+        this.quad.render(renderer);
+        renderer.setRenderTarget(null);
+    }
+    setWeight(value) {
+        this.material.uniforms.uWeight.value = value;
+    }
+    setIntensity(value) {
+        this.material.uniforms.uIntensity.value = value;
+    }
+}
